e52e8585370b4704746a01e3cbe6b3f8a56cbd54
yikangGu <yikangGu@github.com>
2018-10-20 00:13:39 +0800

    complete the l1h2.cpp

--- L1H2/src/L1H2.cpp
+++ L1H2/src/L1H2.cpp
@@ -17,48 +17,82 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
-    cv::Mat B, G, R;
+    // use the split method.
+    // of couse, use the method that converting to hsv frame is also a good method.
+    // base on this method, we know the following code is maybe unuseful for man who use the hsv frame.
+    // but, even so, we can learn some thinking about low-level operation in opencv. 
+    cv::Mat B, R;
     std::vector<cv::Mat> bgrChannels;
     cv::split(src, bgrChannels);
-    R = bgrChannels.at(2);
-    G = bgrChannels.at(1);
     B = bgrChannels.at(0);
+    R = bgrChannels.at(2);
 
-    cv::Mat subRB, binary;
-    subRB = R - B;
-    cv::threshold(subRB, binary, 100, 255, cv::THRESH_BINARY);
+    cv::Mat redBinary;
+    // do not ask me why r - b, cz i just try it out luckily.
+    cv::Mat subRB = R - B;
+    cv::threshold(subRB, redBinary, 100, 255, cv::THRESH_BINARY_INV);
+
+    // backup the src for avoiding the src modified if the following code will modify the input image. 
+    cv::Mat imgR = src.clone();
     cv::Mat imgBG = src.clone();
-    imgBG.setTo(255, binary);
 
-    cv::Mat grayImg;
-    cv::cvtColor(imgBG, grayImg, cv::COLOR_BGR2GRAY);
+    // setTo is an operation from the core of opencv.
+    // this is useful if you can operate the mat directly.
+    // see also : 
+    //   https://docs.opencv.org/3.4.1/d3/d63/classcv_1_1Mat.html#a0440e2a164c0b0d8462fb1e487be9876 
+    imgR.setTo(0, redBinary);
+    imgBG.setTo(0, imgR);
 
-    cv::Mat dilateImg, kernel;
+    // why the cols need multiply 3 times?
+    // you need to think about this.(Ha, in fact, because the mat is an array.)
+    for (int x = 0; x < imgBG.cols * 3; x++)
+        for (int y = 0; y < imgBG.rows; y++)
+            if (imgBG.at<uchar>(y, x) == 255)
+                imgBG.at<uchar>(y, x) = 0;
+
+    cv::Mat grayBG, grayR;
+    cv::cvtColor(imgBG, grayBG, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(imgR, grayR, cv::COLOR_BGR2GRAY);
+
+    cv::Mat erodeImg, kernel;
     kernel = cv::getStructuringElement(CV_SHAPE_RECT, cv::Size(3, 3));
-    cv::dilate(grayImg, dilateImg, kernel, cv::Point(-1, -1), 1);
-
-    // find edge
-    cv::Mat canny;
-    cv::Canny(dilateImg, canny, 100, 200);
+    cv::erode(grayBG, erodeImg, kernel, cv::Point(-1, -1), 1);
 
     std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(canny, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+    cv::findContours(erodeImg, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    int couterBG = contours.size();
+    cv::findContours(grayR, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-    // convect grey to bgr for drawing color in the image.
-    cv::Mat drawing;
-    cv::cvtColor(canny, drawing, cv::COLOR_GRAY2BGR);
+    std::cout << "Objects : " << contours.size() + couterBG << std::endl;
+    
+    // here is too.
+    cv::Mat subBR = B - R;
+    cv::threshold(subBR, subBR, 250, 255, cv::THRESH_BINARY);
+    cv::findContours(subBR, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    std::cout << "blue ball : " << contours.size() << std::endl;
 
-    // contours.size() -1  is means that abandon the biggest one(the last one)
-    for (size_t idx = 0; idx < contours.size() - 1; idx++)
-    {
-        cv::drawContours(drawing, contours, idx, cv::Scalar(0, 0, 255));
-    }
-    std::cout << "couter : " << contours.size() - 1 << std::endl;
+    std::vector<cv::Point> leftBall = contours[1];
+    std::vector<cv::Point> rightBall = contours[2];
+
+    float _;
+    cv::Point2f centerBallLeft;
+    cv::Point2f centerBallRight;
+
+    // you can use the minEnclosing rectangle method,
+    // it doesn't matter, the result is the same in theory.
+    cv::minEnclosingCircle(leftBall, centerBallLeft, _);
+    cv::minEnclosingCircle(rightBall, centerBallRight, _);
+
+    cv::circle(src, centerBallLeft, 1, cv::Scalar(0, 255, 0), 3);
+    cv::circle(src, centerBallRight, 1, cv::Scalar(0, 255, 0), 3);
+    cv::circle(src, cv::Point(
+            (int)((centerBallLeft.x + centerBallRight.x) / 2), 
+            (int)((centerBallLeft.y + centerBallRight.y) / 2)
+        ), 3, cv::Scalar(0, 0, 255), 3);
 
     while (true)
     {
-        cv::imshow("canny", canny);
-        cv::imshow("drawing", drawing);
+        cv::imshow("src", src);
         if (cv::waitKey(1) == 27)
             break;
     }
