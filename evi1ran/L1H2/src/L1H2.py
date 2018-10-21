103c80e454b86fc05f4fbefccbe991a1b54fa8a5
Evi1ran <evilran@cnit.pro>
2018-10-21 17:08:44 +0800

    Revise the code & add a windows to show the contours of all objectives

--- evi1ran/L1H2/src/L1H2.py
+++ evi1ran/L1H2/src/L1H2.py
@@ -16,7 +16,7 @@ image, contours, hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_A
 ret, binary = cv2.threshold(img2, 127, 255, cv2.THRESH_BINARY_INV)
 imageb, contoursb, hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 drawing = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
-
+cv2.drawContours(img, contours[:-1], -1, (0, 0, 255), 3)
 # find the coordinates of lestmost blue point and right most blue point
 x1 = int(cv2.moments(contoursb[1])["m10"] / cv2.moments(contoursb[1])["m00"])
 x2 = int(cv2.moments(contoursb[2])["m10"] / cv2.moments(contoursb[2])["m00"])
@@ -30,6 +30,7 @@ cv2.circle(drawing, (x2, y2), 7, (0, 0, 255), -1)
 cv2.line(drawing, (x1, y1), (x2, y2), (0, 0, 255), 3)
 # print the numbers of all objectives and blue objectives
 print("There are %d objectives, and %d is blue." % (len(contours)-1, len(contoursb)-1))
-cv2.imshow('L1H2', drawing)
+cv2.imshow('L1H2-Task1', img)
+cv2.imshow('L1H2-Task2', drawing)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
