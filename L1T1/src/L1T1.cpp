0d5b5d2a4c677fe05c8216227eea2d00ab4d8bb1
yikangGu <guyk@dgut.edu.cn>
2018-11-13 20:23:35 +0800

    complete cpp

--- L1T1/src/L1T1.cpp
+++ L1T1/src/L1T1.cpp
@@ -17,11 +17,11 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
+    // black ball process
     cv::Mat blackBinary;
     cv::inRange(src, cv::Scalar(82, 72, 65), cv::Scalar(83, 73, 66), blackBinary);
     std::vector<std::vector<cv::Point>> contours;
     cv::findContours(blackBinary, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-    std::cout << "black ball : " << contours.size() << std::endl;
 
     float _;
     cv::Point2f fristBall, secondBall;
@@ -34,6 +34,24 @@ int main(int argc, char const *argv[])
         cv::line(src, cv::Point((int)fristBall.x, (int)fristBall.y),
                  cv::Point((int)secondBall.x, (int)secondBall.y), cv::Scalar(0, 0, 255));
     }
+
+    // find map process
+    cv::Mat mapsrc = src(cv::Range(0, 410),cv::Range(700, src.cols));
+
+    cv::Mat gray;
+    cv::cvtColor(mapsrc, gray, CV_BGR2GRAY);
+
+    cv::Mat canny;
+    cv::Canny(gray, canny, 0, 2);
+
+    cv::Mat dilate, kernel;
+    kernel = cv::getStructuringElement(CV_SHAPE_RECT, cv::Size(2, 2));
+    cv::dilate(canny, dilate, kernel, cv::Point(-1, -1), 1);
+
+    cv::Mat zeros = cv::Mat::zeros(src.rows+2, src.cols+2, CV_8UC1);
+    dilate.copyTo(zeros(cv::Rect(cv::Point(699, 0), mapsrc.size())));
+
+    cv::floodFill(src, zeros, cv::Point(750, 50), cv::Scalar(255, 255, 255));
 
     while (true)
     {
