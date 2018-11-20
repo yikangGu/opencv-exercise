610ab42ccedb0aedbead3dcb7e8ce563d7e799c4
yikangGu <guyk@dgut.edu.cn>
2018-11-20 11:27:37 +0800

    modify some files

--- L3H1/src/L3H1.m
+++ L3H1/src/L3H1.m
@@ -1,4 +1,1 @@
-img = imread('../folder/1.jpg');
-
-subplot(1,2,1);
-imshow(img);
+pass
--- L3H1/src/L3H1.py
+++ L3H1/src/L3H1.py
@@ -3,15 +3,7 @@ import cv2
 
 
 def main():
-    src = cv2.imread("folder/1.png")
-
-    while True:
-        cv2.imshow("src", src)
-
-        if cv2.waitKey(1) == 27:
-            break
-
-    cv2.destroyAllWindows()
+    return 0
 
 
 if __name__ == '__main__':
