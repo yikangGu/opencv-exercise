46ea9d8310ec87d5d8e015e5f0a2b3c3823fbe40
yikangGu <guyk@dgut.edu.cn>
2019-05-03 04:00:13 +0800

    update

--- L1T1/src/L1T1.py
+++ L1T1/src/L1T1.py
@@ -75,19 +75,68 @@ def main():
     map_roi = gray[y:y+h, x:x+w]
 
     # show and draw
-    canny = cv2.Canny(map_roi, 0, 2)
-    canny = cv2.dilate(canny, kernel)
+    map_roi = cv2.Canny(map_roi, 0, 2)
+    map_roi = cv2.dilate(map_roi, kernel)
 
     mask = np.zeros((src.shape[0]+2, src.shape[1]+2), np.uint8)
-    mask[y:y+h, x:x+w] = canny
+    mask[y:y+h, x:x+w] = map_roi
     cv2.floodFill(src, mask, (x, y), (255, 255, 255))
 
+# swap computer monitor
+    canny = cv2.Canny(gray, 127, 255)
+    canny = cv2.dilate(canny, kernel)
+
+    contours, hierarchy = cv2.findContours(
+        canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    for i in range(len(contours)):
+        for j in range(i + 1, len(contours)):
+            if hierarchy[0][i][2] == -1 or hierarchy[0][i][3] == -1:
+                break
+
+            contour_i = cv2.boundingRect(contours[i])
+            contour_j = cv2.boundingRect(contours[j])
+
+            if contour_i[2] < 100 or contour_i[3] < 100:
+                break
+
+            if contour_j[2] < 100 or contour_j[3] < 100:
+                continue
+
+            if cv2.pointPolygonTest(contours[i], (contour_j[0]+(contour_j[2]//2), contour_j[1]+(contour_j[3]//2)), False) != -1:
+                continue
+
+            if not oe.approxLenth((contour_i[2], contour_j[2]), 0.1):
+                continue
+
+            if not oe.approxLenth((contour_i[3], contour_j[3]), 0.1):
+                continue
+
+            source = cv2.matchShapes(
+                contours[i], contours[j], cv2.CONTOURS_MATCH_I1, 0)  # 443, 537
+
+            if source == 0 or source > 1:
+                continue
+
+            left, right = (contour_i, contour_j) if contour_i[0] < contour_j[0] else (
+                contour_j, contour_i)
+
+    left_monitor = src[left[1]:left[1]+left[3], left[0]:left[0]+left[2]]
+    right_monitor = src[right[1]:right[1] +
+                        right[3], right[0]:right[0]+right[2]].copy()
+
+    src[right[1]:right[1]+right[3], right[0]:right[0]+right[2]] = left_monitor
+    src[left[1]:left[1]+left[3], left[0]:left[0]+left[2]] = right_monitor
+
+    cv2.rectangle(
+        src, (left[0], left[1]), (left[0]+left[2], left[1]+left[3]), (0, 255, 0))
+    cv2.rectangle(
+        src, (right[0], right[1]), (right[0]+right[2], right[1]+right[3]), (255, 0, 0))
+
     cv2.imshow("src", src)
-    cv2.imshow("canny", canny)
+    cv2.imshow("map_roi", map_roi)
     cv2.imshow("ipad_roi", ipad_roi)
 
     while True:
-
         if cv2.waitKey(1) == 27:
             break
 
--- L2H1/src/L2H1.py
+++ L2H1/src/L2H1.py
@@ -1,57 +1,6 @@
 import numpy as np
 import cv2
-
-
-def maxLimiter(x, max_x):
-    return max_x if x > max_x else x
-
-
-def minLimiter(x, min_x):
-    return min_x if x < min_x else x
-
-
-def reduce(value, factor):
-    return value * (1 - factor)
-
-
-def enlarge(value, factor):
-    return value * (1 + factor)
-
-
-def isSquare(shape, factor):
-    return (shape[0] > reduce(shape[1], factor)) and (shape[0] < enlarge(shape[1], factor))
-
-
-def get2vsv(pre, cur):
-    return (2*pre[0]-cur[0], 2*pre[1]-cur[1])
-
-
-def extendRect(rect, shape, mode, factor=0.1):
-    '''
-    extendRect(rect, shape, mode, factor) -> retval.
-    @param shape : h, w, (e.g. src.shape)
-    @param mode : if mode = 0, extend by multifly a factor, if mode = 1, extend by add a factor
-    '''
-    max_h, max_w = shape[:2]
-    extend_w, increment_w, extend_h, increment_h = 0, 0, 0, 0
-
-    if mode is 0:
-        extend_w, extend_h = enlarge(rect[2], factor), enlarge(rect[3], factor)
-        increment_w, increment_h = extend_w - rect[2], extend_h - rect[3]
-
-    elif mode is 1:
-        extend_w, extend_h = rect[2] + factor, rect[3] + factor
-        increment_w, increment_h = factor, factor
-
-    else:
-        print(" extendRect MODE ERROR! ")
-
-    w_start = int(minLimiter(rect[0] - increment_w, 0))
-    w_end = int(maxLimiter(rect[0] + extend_w, max_w))
-    h_start = int(minLimiter(rect[1] - increment_h, 0))
-    h_end = int(maxLimiter(rect[1] + extend_h, max_w))
-
-    return (h_start, h_end, w_start, w_end)
+import oe
 
 
 def main():
@@ -79,7 +28,9 @@ def main():
 
         if src is None:
             print("img is empty")
-            print(" accuracy: ", counter*1.0/cur_frame_idx, end='')
+            print(" ***--- ACCURACY:", round(
+                counter * 100.0/cur_frame_idx, 2), "% ---***")
+            counter = 0
             cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
             continue
 
@@ -90,7 +41,7 @@ def main():
 
         if isFollowing is True:
             drawing = cv2.cvtColor(G, cv2.COLOR_GRAY2BGR)
-            hs, he, ws, we = extendRect(preBallRoi, G.shape, 0, 2)
+            hs, he, ws, we = oe.extendRect(preBallRoi, G.shape, 0, 2)
             curBallRoi = G[hs:he, ws:we]
 
             contours, _ = cv2.findContours(
@@ -100,7 +51,7 @@ def main():
             for contour in contours:
                 rect = cv2.boundingRect(contour)
 
-                if isSquare(rect[2:4], 0.5):
+                if oe.approxLenth(rect[2:4], 0.5):
                     cv2.rectangle(drawing, (rect[0], rect[1]), (
                         rect[0]+rect[2], rect[1]+rect[3]), (0, 0, 255))
 
@@ -109,11 +60,12 @@ def main():
                     isFollowing = True
                     preBallRoi = rect
                     counter += 1
-                    print(" following now ")
+                    print(" state: 2 following ")
 
             if checkFind is False:
-                print(" missing ball")
+                print(" state: 3 missing ")
                 isFollowing = False
+                preBalls.clear()
         else:
             drawing = cv2.cvtColor(G, cv2.COLOR_GRAY2BGR)
 
@@ -124,7 +76,7 @@ def main():
             for contour in contours:
                 rect = cv2.boundingRect(contour)
 
-                if isSquare(rect[2:4], 0.5):
+                if oe.approxLenth(rect[2:4], 0.5):
                     cv2.rectangle(drawing, (rect[0], rect[1]), (
                         rect[0]+rect[2], rect[1]+rect[3]), (0, 0, 255))
 
@@ -132,13 +84,13 @@ def main():
                     checkFind = True
                     isFollowing = True
                     preBallRoi = rect
-                    print(" find ")
+                    print(" state: 1 find ")
 
             if checkFind is False:
-                print(" can't find ball")
+                print(" state: -1 can't find ")
 
         if len(preBalls) is 2:
-            curTarget = get2vsv(preBalls[1], preBalls[0])
+            curTarget = oe.get2vsv(preBalls[1], preBalls[0])
 
             cv2.circle(drawing, (preBallRoi[0]+preBallRoi[2]//2,
                                  preBallRoi[1]+preBallRoi[3]//2), 2, (255, 0, 0), -1)
@@ -148,6 +100,7 @@ def main():
             preTarget = curTarget
             preBalls.pop(0)
 
+        print(" target: x:", curTarget[0], " y:", curTarget[1])
         cv2.imshow("G", G)
         cv2.imshow("drawing", drawing)
 
